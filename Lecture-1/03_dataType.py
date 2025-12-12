881eb0003a2bf1e5d88fc3a5f825fe3505f96efa
Anupam Lata Yadav <yanu77662@gmail.com>
2025-12-13 01:28:45 +0530

    Create 03_dataType.py

---
+++ Lecture-1/03_dataType.py
@@ -0,0 +1,23 @@
+# our first program
+
+name =  "anupam"
+age = 35
+PI = 3.14
+
+num = 2
+isPrime = True
+
+isOdd = None
+
+
+print(type(age))
+print(type(PI))
+print(type(name))
+
+print(type(isPrime))
+
+print(type(isOdd))
+
+'''
+this is a multi- line comment
+'''
