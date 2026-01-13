62e3e33f8c8908003e19f22cb5f285a01d1803fa
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-14 00:48:18 +0530

    Create quiz_02.py

---
+++ lecture-1(practice)/quiz_02.py
@@ -0,0 +1,9 @@
+# take two numbers as input from the user and print their sum, difference,product , and quotient.
+
+a = int(input("Enter number A: "))
+b = int(input("Enter number B: "))
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
