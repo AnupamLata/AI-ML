0803384cc1e4d7c3db6179b0be043d6e55cbc85e
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-14 00:50:08 +0530

    Create quiz_05.py

---
+++ lecture-1(practice)/quiz_05.py
@@ -0,0 +1,8 @@
+# evaluate and print the result of the following expression:
+
+# x = 10 + 3 * 2 ** 2 
+# based on what you learnt in the lecture why the output is what it is.
+
+x = 10 + 3 * 2 ** 2
+
+print(x)
