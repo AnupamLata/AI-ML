f62a0339a856cabf39eb4e34877739386333a0a6
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-14 00:48:43 +0530

    Create quiz_03.py

---
+++ lecture-1(practice)/quiz_03.py
@@ -0,0 +1,9 @@
+# ask the user to enter two integers and one float. Convert them all to floats and print their average.
+
+a =float(input("enter first integer: "))
+b =float(input("enter second integer: "))
+c =float(input("enter one float: "))
+
+average = (a+b+c)/3
+
+print("Average", average)
