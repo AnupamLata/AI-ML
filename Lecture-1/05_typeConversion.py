1621f9a42022c8747183b177cf99d81441146879
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-13 20:49:28 +0530

    Create 05_typeConversion.py

---
+++ Lecture-1/05_typeConversion.py
@@ -0,0 +1,23 @@
+# conversion
+
+ans1 = 5 + 10.0
+
+print(ans1) # or 
+print(ans1, type(ans1))
+
+# casting
+
+ans2 = int(5 + 10.0)
+print(type(ans2))
+
+
+# for string 
+val1 = int("123")
+
+print(type(val1))
+
+val2 = bool(0)
+val3 = bool(10)
+
+print(val2, type(val2))
+print(val3, type(val3))
