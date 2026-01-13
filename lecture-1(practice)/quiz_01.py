aee03f207fd4bb4316976d23c95603161af18f40
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-14 00:47:23 +0530

    Create quiz_01.py

---
+++ lecture-1(practice)/quiz_01.py
@@ -0,0 +1,8 @@
+# write a program that asks the user for their name and age, then prints a sentence like:
+# Hello Anupam , you are 21 years old !
+
+name = str(input("enter the name : "))
+
+age = int(input("enter the age: "))
+
+print("Hello" , name, "," ,"you are", age , "year old!")
