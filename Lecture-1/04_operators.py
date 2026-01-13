a14c96b65686307e15a040fab6f72315b6d8907c
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-13 20:48:41 +0530

    Create 04_operators.py

---
+++ Lecture-1/04_operators.py
@@ -0,0 +1,50 @@
+#arithmetic opertors
+
+
+# a = 10
+# b = 5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)  # modulo
+# print(a**b)
+
+
+# # relational / comparison
+
+# a = 10
+# b = 5
+# print(a < b)
+# print(a > b)
+# print(a == b)
+# print(a != b)
+# print(a <= b)
+
+# # assignment operator
+
+# a = 10
+# # a = a+5
+# # or
+# a += 5
+
+# b = 5
+# b -= 10
+
+# print(a)
+# print(b)
+
+
+# logical operator
+# not
+var = False
+
+print(not var)
+print(not (5 > 8))
+print((5 > 8))
+
+# and
+print((5 > 3) and (3 > 8))
+
+# or
+print((5 > 3) or (3 > 8))
