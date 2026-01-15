b56ce76fcbaae1d58a42d79664bc519e2cf61345
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-15 22:32:43 +0530

    Create 03_elif_statement.py

---
+++ Lecture-2/03_elif_statement.py
@@ -0,0 +1,13 @@
+# traffic lights
+
+color = input("enter color: ")
+
+if color == "red":
+    print("stop")
+elif color == "green":
+    print("go")
+elif color == "yellow":
+    print("look")
+else:
+    print("worng color for traffic lights")
+
