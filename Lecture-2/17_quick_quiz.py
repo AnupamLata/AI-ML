4b5436cda9342c7824709f8347eb1b8b8edbbbe6
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-18 21:38:24 +0530

    Create 17_quick_quiz.py

---
+++ Lecture-2/17_quick_quiz.py
@@ -0,0 +1,18 @@
+# print all the ODD numbers from 1 to 10
+
+i = 0
+
+while(i < 10):
+    i += 1
+    if(i % 2 == 0):
+    
+        continue  #skip
+
+    print(i)
+    
+
+    # or
+    #  i = 1
+    # while (i <= 10):
+    #     print(i)
+    #     i += 2
