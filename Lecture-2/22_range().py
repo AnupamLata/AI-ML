9a8c71f396abb56e82e02d390fc20a2c745d9ae5
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-18 22:02:34 +0530

    Create 22_range().py

---
+++ Lecture-2/22_range().py
@@ -0,0 +1,18 @@
+# loop
+# range () function
+
+for i in range(5):
+    print(i)
+
+for k in range(1, 6):
+    print(k)    
+
+
+#  print ODD no's
+for j in range(1, 10, 2):
+    print(j)  
+
+# print EVEN no's   
+for l in range(2, 11, 2):
+    print(l)
+
