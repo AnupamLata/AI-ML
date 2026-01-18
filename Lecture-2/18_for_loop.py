d7741df2150e2e1f5be7619839347ae3357e8e40
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-18 21:38:57 +0530

    Create 18_for_loop.py

---
+++ Lecture-2/18_for_loop.py
@@ -0,0 +1,14 @@
+# strings for loop
+
+string = "Hello"
+
+# in => membership operator
+
+for var in string:
+    print(var)
+
+
+    #  check presence
+
+# if 'o' in string:
+#     print("exits in string")
