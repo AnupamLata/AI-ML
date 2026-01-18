2f7efcb3d6da412c03758bce39ca3496b8f1e59f
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-18 21:59:48 +0530

    Create 20_quick_quiz.py

---
+++ Lecture-2/20_quick_quiz.py
@@ -0,0 +1,11 @@
+# count the number of i's
+# word = "artifical intelligence"
+
+word = "artificial intelligence"
+
+count = 0
+for ch in word:
+    if(ch == 'i'):
+        count += 1
+
+print("count of i's = ", count)
