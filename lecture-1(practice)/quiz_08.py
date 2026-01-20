883e10399aead5c5720b0c4e94b523fcd4a0e227
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-20 21:50:07 +0530

    Create quiz_08.py

---
+++ lecture-1(practice)/quiz_08.py
@@ -0,0 +1,9 @@
+# Take the radius (r) as user input and print the area.
+# use the formula: Area = π * r**2    .  (value of π = 3.14)
+
+radius = float(input("Enter the radius: "))
+
+pi = 3.14
+Area = pi * radius**2
+
+print(Area)
