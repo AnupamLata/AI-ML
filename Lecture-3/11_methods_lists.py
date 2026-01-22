96774fa750b33f4cb92cd9c28477a36035b2e7bb
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-22 23:35:46 +0530

    Create 11_methods_lists.py

---
+++ Lecture-3/11_methods_lists.py
@@ -0,0 +1,26 @@
+# methods of lists
+
+nums = [1,2,3,6,8,4]
+
+# I.append(val) -> add one element at the end
+nums.append(7)
+print(nums)
+
+
+# I.insert(idx,val) -> insert element at idx
+nums.insert(2, 10)
+print(nums)
+
+
+# I.sort()  -> arranges in increasing order
+nums.sort()
+print(nums)
+#  or
+nums.sort(reverse = True)
+print(nums)
+
+
+# i.reverse() -> reverse order
+nums.reverse()
+print(nums)
+
