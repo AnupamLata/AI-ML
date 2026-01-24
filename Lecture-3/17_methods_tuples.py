a2904fb8ee12c6fe72156a911ca5486e98694061
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-24 21:24:53 +0530

    Create 17_methods_tuples.py

---
+++ Lecture-3/17_methods_tuples.py
@@ -0,0 +1,10 @@
+# methods of tuples
+
+# for t.index(val)  -> return 1 occurance idx
+tup = (1, 3, 2, 3, 2, 3, 3)
+
+print(tup.index(3))
+
+# for c.count(val)  -> count total occurance
+
+print(tup.count(3))
