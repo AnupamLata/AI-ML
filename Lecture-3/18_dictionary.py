c267317486cef0fb75c7119ed724687129215520
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-24 21:25:42 +0530

    Create 18_dictionary.py

---
+++ Lecture-3/18_dictionary.py
@@ -0,0 +1,15 @@
+# dictionary
+
+info = {
+    "name" : "Anupam",
+    "cgpa" : 9.2,
+    "subject " : ["math","science"],
+    3.14 : "PI"
+}
+
+info["cgpa"] = 9.6
+
+print(info["cgpa"])
+print(info)
+print(info["name"])
+print(type(info))
