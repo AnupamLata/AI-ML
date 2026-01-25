742696251367772e75a37e9ad58d83d96ea5d3d4
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-25 21:21:46 +0530

    Create 21_quick_quiz.py

---
+++ Lecture-3/21_quick_quiz.py
@@ -0,0 +1,10 @@
+s = {1,2,2,2,3}
+
+s.add(5)
+empty_set = {}  # this create a empty dictorary NOT empty sets
+
+empty_sets = set()   #this create a empty sets
+
+print(s)
+print(type(empty_set))
+print(type(empty_sets))
