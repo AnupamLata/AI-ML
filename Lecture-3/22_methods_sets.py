b4912b2834725fd3d08a74e2dae9b1c56524d6bc
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-25 21:22:31 +0530

    Create 22_methods_sets.py

---
+++ Lecture-3/22_methods_sets.py
@@ -0,0 +1,25 @@
+# # methods of sets
+
+s = {1, 2, 2, 2, 3, 2}
+
+# s.add(val)  -> adda a val
+s.add(5)
+print(s)
+
+
+
+# s.remove(val)  -> remove a val
+s.remove(3)
+print(s)
+
+
+# s.clear()   -> empties the set
+s.clear()
+print(s)
+
+
+# s.pop()  -> removes a random val
+s.pop()
+print(s)
+
+
