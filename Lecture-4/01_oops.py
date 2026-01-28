547e173b287f0de447f5d1ca9ba0687279b8318a
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-28 21:32:06 +0530

    Create 01_oops.py

---
+++ Lecture-4/01_oops.py
@@ -0,0 +1,14 @@
+# opps
+class student:
+    subject = "python"
+    college = "ABC"
+    year = "4th year"
+
+stu1 = student()
+stu2 = student()
+
+print(stu1)
+print(stu2)
+
+print(stu1.subject, stu1.college, stu1.year)
+print(stu2.subject, stu2.college, stu2.year)
