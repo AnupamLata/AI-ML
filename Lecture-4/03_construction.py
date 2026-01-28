4dea5c661c3f2c9bfbaec6d8de273956beb568f8
Anupam Lata Yadav <yanu77662@gmail.com>
2026-01-28 21:33:28 +0530

    Create 03_construction.py

---
+++ Lecture-4/03_construction.py
@@ -0,0 +1,10 @@
+# construction
+#  init_method
+
+class Student:
+    def __init__(self):  # default constructor
+        print("constructor was called...")
+
+stu1 = Student()        
+stu2 = Student()        
+stu3 = Student()        
