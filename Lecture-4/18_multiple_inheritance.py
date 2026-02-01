fac2445e252b6d09c99a9b4606dd7b785fcfa738
Anupam Lata Yadav <yanu77662@gmail.com>
2026-02-01 19:42:18 +0530

    Implement multiple inheritance with Teacher and Student

---
+++ Lecture-4/18_multiple_inheritance.py
@@ -0,0 +1,17 @@
+class Teacher:
+    def __init__(self, salary):
+        self.salary = salary
+
+class Student:
+    def __init__(self, gpa):
+        self.gpa = gpa
+
+class TA(Teacher, Student):
+    def __init__(self, salary, gpa, name):
+        super().__init__(salary)
+        Student.__init__(self, gpa)
+        self.name = name
+
+ta1 = TA(1000 , 9.4, "Anupam")
+
+print(ta1.name, ta1.gpa, ta1.salary)
