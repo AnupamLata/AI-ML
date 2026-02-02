d0e0c2e265028ace76c42560be77648c853e70c9
Anupam Lata Yadav <yanu77662@gmail.com>
2026-02-02 21:07:42 +0530

    Create 21_function_overriding.py

---
+++ Lecture-4/21_function_overriding.py
@@ -0,0 +1,12 @@
+# function overriding
+
+class Employee:
+    def get_designation(self):
+        print("designation = Employee")
+
+class Teacher(Employee):
+    def get_designation(self):
+        print("designation = Teacher")    
+
+t1 = Teacher()
+t1.get_designation()            
