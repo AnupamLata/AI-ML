062ece8af25f11654abb3bf67596c60e83d53f60
Anupam Lata Yadav <yanu77662@gmail.com>
2026-02-05 19:04:37 +0530

    Add exception handling for division and input errors

---
+++ Lecture-5/09_exception_handling.py
@@ -0,0 +1,13 @@
+try:
+    x = int(input("enter x: "))
+    ans = 10/x
+
+except ZeroDivisionError:
+    print(f"divide by 0 is not allowed")
+
+except ValueError:
+    print(f"Invalid input")
+
+else:
+    print(f"ans = {ans}")    
+
