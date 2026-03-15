2789f13768bc94087070e35160f58d9e5e025067
Anupam Lata Yadav <yanu77662@gmail.com>
2026-03-15 11:24:59 +0530

    Delete lecture-1(practice)/quiz_13.py

--- lecture-1(practice)/quiz_13.py
+++
@@ -1,8 +0,0 @@
-# Create a class Person with a method display() that prints "Hello, Person! " . Create an object and call the method.
-
-class Person:
-    def display(self):
-        print("Hello, Person!")
-
-p1 = Person()
-p1.display()       
